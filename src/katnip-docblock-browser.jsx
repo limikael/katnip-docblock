7f8e7de4c0a32d690dec78d76edfc8844e1aadee
Mikael Lindqvist <li.mikael@gmail.com>
2022-12-04 21:04:31 +0800

    bla

--- src/katnip-docblock-browser.jsx
+++ src/katnip-docblock-browser.jsx
@@ -2,9 +2,6 @@ import {katnip} from "katnip";
 import FILE_EARMARK_TEXT from "bootstrap-icons/icons/file-earmark-text.svg";
 import DocumentationView from "../components/DocumentationView.jsx";
 
-console.log(katnip);
-console.log(katnip.createCrudUi);
-
 katnip.createCrudUi("documentation",{
 	columns: {
 		url: {label: "Url"},
